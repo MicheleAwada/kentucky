13463b0d9a9c90409ae3029bab8c5d268428553d
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-11-25 23:12:50 +0300

    new art, added gliding, eating, charging jump, etc

--- kaboom/static/kaboom/game.js
+++ kaboom/static/kaboom/game.js
@@ -1,150 +1,304 @@
 kaboom({
-    width: 1000,
-    height: 600,
-    debug: true,
-    global:true,
-    background: [135, 206, 235],
+	width: 1000,
+	height: 600,
+	debug: true,
+	global: true,
+	background: [135, 206, 235],
 });
 
-/* window.addEventListener('resize', () => {
-    kaboom.setCanvasDimensions(window.innerWidth, window.innerHeight * 0.5);
-  }); */
+loadRoot("static/kaboom/images/");
+loadSprite("kentucky", "kentucky spritesheet.png", {
+	sliceX: 15,
+	anims: {
+		walk: { from: 0, to: 3 },
+		jump_charge: { from: 4, to: 7 },
+		max_jump_charge: { from: 8, to: 11 },
+		jump: { from: 0, to: 0 },
+    glide: { from: 12, to: 13 },
+    eat: { from: 14, to: 14 },
+	},
+});
+// loadSprite("fox", "fox.png")
+loadSprite("fox", "fox spritesheet.png", {
+	sliceX: 2,
+	anims: {
+		walk: {
+			from: 0,
+			to: 1,
+		},
+	},
+});
 
-// loadRoot("static/kaboom/")
-loadRoot("static/kaboom/images/")
-loadSprite("kentucky", "kentucky.png")
-loadSprite("kentucky1", "kentucky1.png")
-loadSprite("kentucky2", "kentucky2.png")
-loadSprite("fox", "fox.png")
 
+//food
+loadSprite("banana", "food/banana.png")
 
-const floor_height = 40
-//TODO when resized
-const floor_y = height()-floor_height
+const floor_height = 40;
+// TODO when resized // actually maybe not needed
+const floor_y = height() - floor_height;
 
-const floor =add([
-  pos(0, floor_y),
-  rect(width(), floor_height),
-  area(),
-  color(120,120,120),
-  body({ isStatic: true }),
-])
-
-const kentucky_image_height = 175
-
-setGravity(1000)
-
-function gravity() {
-  let acc_y = 0;
-  let jump_charge = 0;
-  const jump_charge_max = 10
-  return {
-    update() {
-      if (this.pos.y > floor_y) {
-        this.pos.y = floor_y
-      }
-      else {
-        this.pos.y = Math.min(floor_y, this.pos.y + acc_y)
-        if (this.pos.y < floor_y) {
-          acc_y++
-        }
-      }
-      if (jump_charge) {
-        this.charge_jump()
-      }
-    },
-    jump(force=Math.floor(jump_charge/2), extra=15) {
-      if (this.pos.y === floor_y) {
-        acc_y = -(force+extra)
-      }
-      jump_charge = 0
-    },
-
-    charge_jump() {
-      if (jump_charge>jump_charge_max) {
-        return jump_charge
-      }
-      return ++jump_charge
-    },
-    charged_jump_is_max() {
-      return jump_charge >= jump_charge_max
-    }
-  }
-}
-
-const player = add([
-  sprite("kentucky1"),
-  pos(width()-75, floor_y-200),
-  area(),
-  gravity(),
-  scale(5),
-  anchor("botright"),
-  {
-    dir: LEFT,
-    dead: false,
-},
+const floor = add([
+	pos(0, floor_y),
+	rect(width(), floor_height),
+	area(),
+	color(120, 120, 120),
+	body({ isStatic: true }),
 ]);
 
-onKeyPress("space", () => {
-  player.charge_jump()
+setGravity(1000);
+
+function gravity() {
+	let acc_y = 0;
+	let is_air = false;
+  let gliding = false;
+
+	let jump_charge = 0;
+	let jump_charge_maxed = false;
+  let wait_till_ground = false;
+
+	const base_jump_amplifier = 1.1;
+	const base_jump_force_amplifier = 6.5;
+	const base_jump_extra_amplifier = 1;
+	const jump_charge_max_time = 2.5;
+	const jump_charge_extra = 20;
+
+	const base_gravity_amplifier = 60;
+  const base_gliding_gravity_amplifier = 10;
+  let gravity_amplifier = base_gravity_amplifier
+	return {
+		update() {
+			if (this.pos.y > floor_y) { //below ground
+				this.pos.y = floor_y;
+			} else { //on or above ground
+        
+        // gravity logic
+				this.pos.y = Math.min(
+					floor_y,
+					this.pos.y + acc_y * gravity_amplifier * dt()
+				);
+
+        // above ground
+				if (this.pos.y < floor_y) {
+					acc_y++;
+				} else {
+          if (is_air) {
+            if (gliding) {
+              gliding = false;
+              gravity_amplifier = base_gravity_amplifier
+            }
+            stop_n_animate(this, kentucky_animation_speed, "walk");
+            is_air = false;
+          }
+          if (wait_till_ground) {
+            wait_till_ground = false;
+            this.glide_or_charge_jump();
+          }
+				}
+			}
+			if (jump_charge) {
+				this.charge_jump();
+			}
+		},
+		jump(
+			force = Math.floor(jump_charge),
+			extra = jump_charge_extra,
+			amplifier = 1
+		) {
+			force *= amplifier * base_jump_amplifier * base_jump_force_amplifier;
+			extra *= amplifier * base_jump_amplifier * base_jump_extra_amplifier;
+			if (this.pos.y === floor_y) {
+				acc_y = -(force + extra);
+			}
+			is_air = true;
+			jump_charge = 0;
+			jump_charge_maxed = false;
+
+			stop_n_animate(this, kentucky_animation_speed, "jump");
+		},
+		glide_or_charge_jump() {
+			if (!is_air) {
+				stop_n_animate(this, kentucky_animation_speed, "jump_charge");
+				this.charge_jump();
+			} else {
+				wait_till_ground = true;
+        gliding = true;
+        gravity_amplifier = base_gliding_gravity_amplifier
+        stop_n_animate(this, kentucky_animation_speed, "glide");
+			}
+		},
+		charge_jump() {
+			if (this.charged_jump_is_max()) {
+				!jump_charge_maxed &&
+					stop_n_animate(this, kentucky_animation_speed, "max_jump_charge");
+				jump_charge_maxed = true;
+			} else {
+				jump_charge = jump_charge + dt();
+			}
+			return jump_charge;
+		},
+		charged_jump_is_max() {
+			return jump_charge >= jump_charge_max_time;
+		},
+    dont_wait_till_ground() {
+      wait_till_ground = false;
+    },
+    is_grounded() {
+      return !is_air; 
+    },
+    is_normal() {
+      return this.is_grounded() && jump_charge === 0 // TODO && not crouched
+    },
+    stop_glide() {
+      glide = false
+      gravity_amplifier = base_gravity_amplifier
+      stop_n_animate(this, kentucky_animation_speed, "jump");
+    }
+	};
+}
+
+function scale_for_image(base, desired) {
+	return desired / base;
+}
+
+const base_kentucky_image_height = 175;
+const scale_for_kentucky = scale_for_image(base_kentucky_image_height, 875);
+
+const kentucky_speed = 250; //basically just the speed of "static" objects to make kentucky look moving
+const kentucky_animation_speed = 4;
+
+const player = add([
+	sprite("kentucky"),
+	pos(width() - 75, floor_y - 200),
+	area(),
+	gravity(),
+	scale(scale_for_kentucky),
+	anchor("botright"),
+  "player",
+  "kentucky",
+	{
+		dir: LEFT,
+		dead: false,
+	},
+]);
+
+animate(player, kentucky_animation_speed);
+
+onCollide("player", "food", (p,f) => {
+  f.destroy();
+  if (p.is_normal()) {
+  stop_n_animate(p, 0.5, "eat")
+  }
+  setTimeout(() => {
+    if (p.is_normal()) {
+      stop_n_animate(p, kentucky_animation_speed, "walk")
+    }
+  }, 156)
+
 })
 
-onKeyRelease("space", () => {
-  player.jump();
-
+onKeyPress("space", () => {
+	player.glide_or_charge_jump();
 });
 
-
-
-
-
+onKeyRelease("space", () => {
+  player.dont_wait_till_ground();
+  player.stop_glide();
+	player.jump();
+});
 
 onUpdate("bad", (b) => {
-  b.move(440,0)
-  if (b.pos.x>width()+400) {
-  b.destroy()
+	b.move(600, 0);
+	if (b.pos.x > width() + 400) {
+		b.destroy();
+	}
+});
 
-  }
-})
-
-
-
-function addFox() {
-  return add([
-    sprite("fox"),
-    pos(0, floor_y),
-    anchor("botright"),
-    area(),
-    scale(0.35),
-    "bad",
-    {
-      dir: RIGHT,
-      dead: false,
+function move_obstacle(speed = kentucky_speed, bad=false, flying = false, wiggle = false) {
+  let killed=false;
+	return {
+    update() {
+      if (!killed) {this.move(speed,0);
+      if (this.pos.x> width() + 400) {// plus 400 just because of image width so it doesnt go away before it goes offscreen
+        this.destroy();
+      }}
+    },
+    killed() {
+      killed=true
     }
-  ])
+  }
 }
 
-function random_boolean_with_weight(weight) {
-  const random_weight = Math.min(weight/10, 0.8) * 0.5
-  const random_decimal = Math.random()
-  return Math.random() < random_weight
+function stop_n_animate(obj, speed = 3.5, sprite_animation_name = "walk") {
+	obj.stop();
+	return animate(obj, speed, sprite_animation_name);
 }
 
-let weight = 6;
+function animate(obj, speed = 3.5, sprite_animation_name = "walk") {
+	return obj.play(sprite_animation_name, {
+		loop: true, // Enable looping
+		speed: speed, // Speed of animation (150ms interval)
+	});
+}
+
+function summonFox() {
+	const fox = add([
+		sprite("fox"),
+		pos(0, floor_y),
+		anchor("botright"),
+		area(),
+		scale(3.5),
+		"bad",
+		{
+			dir: RIGHT,
+			dead: false,
+		},
+	]);
+	animate(fox);
+	return fox;
+}
+function summonFood(animate_funciton=null) {
+	const food = add([
+		sprite("banana"),
+		pos(0, floor_y),
+		anchor("botright"),
+		area(),
+		scale(3.5),
+    move_obstacle(),
+		"food",
+		"good",
+		{
+			dir: RIGHT,
+		},
+	]);
+	animate_funciton && animate_funciton();
+	return food;
+}
 
 
-let loop_count = 0
-let loop_add = 24 //meaning how often a obs is created, in this case every 16 loops
 
-const loop_add_max = 245
-const loop_add_min = 12
+let loop_count = 1;
+let loop_add = 45; //meaning how often a obs is created, in this case every 16 loops
 
-const obs_creation = setInterval(() => {
-  if (loop_count % loop_add) {
-    if (loop_count % Math.min(loop_add_add_max,loop_add*loop_add)) {
-      loop_add = Math.max(loop_add-1, loop_add_min)
-    }
-    addFox()
-  }
-  loop_count++
-}, 100)
+const loop_change_add_max = 256;
+const loop_change_add_min = 12;
+
+const loop_animate = 5;
+
+const obs_manipulation = setInterval(() => {
+	if (loop_count % loop_add === 0) {
+		// making duration less as you play more to summon foxes
+		if (loop_count % Math.min(loop_change_add_max, loop_add * loop_add) === 0) {
+			loop_add = Math.max(loop_add - 1, loop_change_add_min);
+		}
+		summonFood();
+	}
+
+	if (loop_count % loop_animate === 0) {
+		let animate_type = loop_count % (loop_animate * 2) === 0;
+		// every("two_animate", (obj) => {
+		//   obj.use(sprite(obj.two_animate_base + animate_type ? '1' : '2'))
+		// })
+	}
+
+	loop_count++;
+}, 100);
